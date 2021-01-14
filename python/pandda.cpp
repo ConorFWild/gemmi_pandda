54e3c95fbd671335448236d55dc493ae0c220fce
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2021-01-14 16:45:43 +0000

    t

--- python/pandda.cpp
+++ python/pandda.cpp
@@ -22,6 +22,63 @@ namespace py = pybind11;
 using namespace gemmi;
 
 
+Grid<float> interpolate_to_list(
+    Grid<float> moving_map,
+    std::vector<std::vector<int>> point_vec,
+    std::vector<std::vector<double>> pos_vec,
+    std::vector<Transform> transform_vec,
+    std::vector<std::vector<double>> com_moving_vec,
+    std::vector<std::vector<double>> com_reference_vec
+)
+{
+
+    std::vector<double> vals(point_vec.size());
+
+    for (std::size_t i=0; i < point_vec.size(); i++)
+    {
+        // Position
+        std::vector<int> point = point_vec[i];
+
+        Fractional fractional = Fractional(
+            point[0] * (1.0 / interpolated_map.nu), 
+            point[1] * (1.0 / interpolated_map.nv), 
+            point[2] * (1.0 / interpolated_map.nw)
+            );
+        Position pos = interpolated_map.unit_cell.orthogonalize(fractional);
+        Transform transform = transform_vec[i];
+        std::vector<double> com_moving = com_moving_vec[i];
+        std::vector<double> com_reference = com_reference_vec[i];
+
+        //Subtract reference com
+        pos.x -= com_reference[0];
+        pos.y -= com_reference[1];
+        pos.z -= com_reference[2];
+
+        //transform
+        Position pos_moving = Position(transform.apply(pos));
+
+        // add moving com
+        pos_moving.x += com_moving[0];
+        pos_moving.y += com_moving[1];
+        pos_moving.z += com_moving[2];
+
+        // fractionalise
+        Fractional pos_moving_fractional = moving_map.unit_cell.fractionalize(pos_moving);
+
+        // interpolate
+        float interpolated_value = moving_map.interpolate_value(pos_moving_fractional);
+
+        // assign
+        vals[i] = interpolated_value;
+
+
+    };
+
+    return vals;
+
+
+}
+
 Grid<float> interpolate_points(
     Grid<float> moving_map,
     Grid<float> interpolated_map, 
@@ -86,6 +143,12 @@ void add_pandda(py::module& m) {
     m.def(
         "interpolate_points", 
         &interpolate_points, 
+        "Interpolates a list of points."
+    );
+
+    m.def(
+        "interpolate_to_list", 
+        &interpolate_to_list, 
         "Interpolates a list of points."
     );
 

2c0873c1299c557523dd2c17d9ad503c88a8e5f4
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2021-01-27 17:04:16 +0000

    t

--- python/pandda.cpp
+++ python/pandda.cpp
@@ -92,8 +92,13 @@ Grid<float> interpolate_points(
 {
     for (std::size_t i=0; i < point_vec.size(); i++)
     {
+
+        // Release gil for threading support
+        py::gil_scoped_release release;
+
         // Position
         std::vector<int> point = point_vec[i];
+        cout << "Point: " << point[0] << " " << point[1] << " " << point[2];
 
         Fractional fractional = Fractional(
             point[0] * (1.0 / interpolated_map.nu), 
@@ -102,8 +107,16 @@ Grid<float> interpolate_points(
             );
         Position pos = interpolated_map.unit_cell.orthogonalize(fractional);
         Transform transform = transform_vec[i];
+        cout << "Trasform: " << transform.vec[0] << " " << transform.vec[1] << " " << transform.vec[2];
+        cout << "Trasform: " << transform.mat[0][0] << " " << transform.mat[0][1] << " " << transform.mat[0][2];
+        cout << "Trasform: " << transform.mat[1][0] << " " << transform.mat[1][1] << " " << transform.mat[1][2];
+        cout << "Trasform: " << transform.mat[2][0] << " " << transform.mat[2][1] << " " << transform.mat[2][2];
+        
+
         std::vector<double> com_moving = com_moving_vec[i];
+        cout << "com_moving: " << com_moving[0] << " " << com_moving[1] << " " << com_moving[2];
         std::vector<double> com_reference = com_reference_vec[i];
+        cout << "com_reference: " << com_reference[0] << " " << com_reference[1] << " " << com_reference[2];
 
         //Subtract reference com
         pos.x -= com_reference[0];
@@ -139,6 +152,208 @@ Grid<float> interpolate_points(
 
 
 }
+
+
+std::vector<Fractional> points_to_positions(
+    Grid<float> interpolated_map, 
+    std::vector<std::vector<int>> point_vec
+    )
+{
+
+    std::vector<Fractional> position_vec(position_vec.size());
+
+    for (std::size_t i=0; i < point_vec.size(); i++)
+    {
+        // Position
+        std::vector<int> point = point_vec[i];
+
+        Fractional fractional = Fractional(
+            point[0] * (1.0 / interpolated_map.nu), 
+            point[1] * (1.0 / interpolated_map.nv), 
+            point[2] * (1.0 / interpolated_map.nw)
+            );
+        Position pos = interpolated_map.unit_cell.orthogonalize(fractional);
+        
+        position_vec[i] = pos;
+
+    }
+
+    return position_vec;
+
+}
+
+std::vector<Fractional> transform_positions(
+    position_vec,
+    transform_vec,
+    com_moving_vec,
+    com_reference_vec
+)
+{
+    std::vector<Fractional> fractional_vec(position_vec.size());
+    
+    for (std::size_t i=0; i < position_vec.size(); i++)
+    {
+        // Position
+        Position pos = position_vec[i];
+        Transform transform = transform_vec[i];
+        std::vector<double> com_moving = com_moving_vec[i];
+        std::vector<double> com_reference = com_reference_vec[i];
+
+        //Subtract reference com
+        pos.x -= com_reference[0];
+        pos.y -= com_reference[1];
+        pos.z -= com_reference[2];
+
+        //transform
+        Position pos_moving = Position(transform.apply(pos));
+
+        // add moving com
+        pos_moving.x += com_moving[0];
+        pos_moving.y += com_moving[1];
+        pos_moving.z += com_moving[2];
+
+        // fractionalise
+        Fractional pos_moving_fractional = moving_map.unit_cell.fractionalize(pos_moving);
+
+        fractional_vec[i] = pos_moving_fractional;
+
+    }
+
+    return fractional_vec;
+}
+
+Grid<float> interpolate_points_from_positions(
+    Grid<float> moving_map,
+    Grid<float> interpolated_map, 
+    std::vector<std::vector<int>> point_vec,
+    std::vector<Fractional> position_vec
+    )
+{
+    // Release gil for threading support
+    py::gil_scoped_release release;
+
+    // Iterate over the corresponding points and positions to interpolate at 
+    for (std::size_t i=0; i < position_vec.size(); i++)
+    {
+        std::vector<int> point = point_vec[i];
+
+        Fractional position = position_vec[i];
+
+        // interpolate
+        float interpolated_value = moving_map.interpolate_value(position);
+
+        // assign
+        interpolated_map.set_value(
+            point[0],
+            point[1],
+            point[2],
+            interpolated_value
+            );
+
+
+    };
+
+    return interpolated_map;
+
+}
+
+
+
+// Grid<float> interpolate_points(
+//     Grid<float> moving_map,
+//     Grid<float> interpolated_map, 
+//     std::vector<std::vector<int>> point_vec,
+//     std::vector<std::vector<double>> pos_vec,
+//     std::vector<Transform> transform_vec,
+//     std::vector<std::vector<double>> com_moving_vec,
+//     std::vector<std::vector<double>> com_reference_vec
+//     )
+// {
+
+
+
+//     Grid<float> interpolated_map = interpolate_points_from_positions(
+//             Grid<float> moving_map,
+//             Grid<float> interpolated_map, 
+//             std::vector<std::vector<int>> point_vec,
+//             std::vector<Fractional> position_vec
+//             );
+
+//     return interpolated_map;
+// }
+
+// float get_rmsd(
+//     std::vector<float>,
+//     std::vector<float>,
+// )
+// {
+
+
+// }
+
+// float optimise_stochastic_downhill(
+//     std::function<float(float)> func,
+//     float step,
+//     float start
+// )
+// {
+
+// }
+
+// std::vector<float> apply_exponential_scale(
+//     std::vector<float> array,
+// )
+// {
+
+
+// }
+
+// struct Solver
+// {
+
+
+//     std::vector<float> reference_array;
+//     std::vector<float> moving_array;
+//     std::vector<float> working_array;
+//     std::vector<float> resolution_array;
+
+
+//     void operator()(float scale)
+//         {
+            
+//             reset_working_array(working_array, moving_array);
+
+//             apply_exponential_scale(scale, working_array);
+
+//             float rmsd = get_rmsd(reference_array);
+
+//             return rmsd; 
+
+//         }
+// };
+
+// float calculate_isotropic_b_factor_scale(
+//     std::vector<float> reference_array,
+//     std::vector<float> moving_array,
+//     std::vector<float> resolution_array
+// )
+// {
+
+//     Solver solver;
+//     solver.reference_array ;
+//     solver.moving_array ;
+//     solver.working_array ;
+//     solver.resolution_array ;
+    
+
+//     scaling = optimise_stochastic_downhill(solver, 0.05, 0.0);
+
+//     scaled_array = apply_exponential_scale(scaling, moving_array);
+
+//     return scaling; 
+
+// }
+
 
 void add_pandda(py::module& m) {
     m.def(
